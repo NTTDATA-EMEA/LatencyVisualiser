e79ce837a54c6b596fd078e5cb37a83c5a4db6a8
Oliver Koeth <Oliver.Koeth@nttdata.com>
2020-12-09 15:29:27 +0100

    Initial Commit

---
+++ LatencyVisualiser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LatencyVisualiser
+//
+//  Created by Oliver Köth on 09.12.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LatencyVisualiser/LatencyVisualiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  LatencyVisualiserApp.swift
+//  LatencyVisualiser
+//
+//  Created by Oliver Köth on 09.12.20.
+//
+
+import SwiftUI
+
+@main
+struct LatencyVisualiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
